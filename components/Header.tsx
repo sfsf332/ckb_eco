1b2fb6c3bcac460884d7e799c0133b244c7fac6a
Jack <k4667333@gmail.com>
2024-06-07 13:26:54 +0800

    about

--- components/Header.tsx
+++ components/Header.tsx
@@ -39,7 +39,7 @@ export default function Header() {
               Community
             </Link>
             <Link
-              href="/"
+              href="/about"
               className={router.pathname === "/about" ? "sel" : ""}
             >
               About
