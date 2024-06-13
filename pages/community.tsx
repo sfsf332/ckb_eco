0907250bf03017fe293bd119a1a12951c51b0151
Jack <k4667333@gmail.com>
2024-06-14 05:21:39 +0800

    chole

--- pages/community.tsx
+++ pages/community.tsx
@@ -6,7 +6,7 @@ import { Footer } from "../components/Footer";
 import localFont from "next/font/local";
 import AnimatedAccordion from "components/AnimatedAccordion";
 import React, { useEffect } from "react";
-import NewsList from "../public/json/news.json";
+import ReportList from "../public/json/report.json";
 import Events from "../public/json/event.json";
 import { FaTwitter, FaGlobe } from "react-icons/fa";
 
@@ -20,43 +20,93 @@ const RanadeFont = localFont({
 });
 
 const Community = () => {
-  const [showNews, setShowNews] = React.useState(NewsList);
+  const [showNews, setShowNews] = React.useState(ReportList);
   const [showEvents, setShowEvents] = React.useState(Events);
-
- 
- 
   return (
     <>
       <Header />
       <main className={"comm-main " + RanadeFont.className}>
         <div className="comm-block-1">
           <h2 className={sharpieFont.className}>
-          One Community,<br />
-          Infinite Possibilities
+            One Community,
+            <br />
+            Infinite Possibilities
           </h2>
           <h4>
-          Stay up-to-date with the latest news, participate in events, and connect with fellow CKB enthusiasts through our community.
+            Stay up-to-date with the latest news, participate in events, and
+            connect with fellow CKB enthusiasts through our community.
           </h4>
-         
         </div>
         <div className="comm-block-2">
-          <div className="comm-title">
-            <h2 className={sharpieFont.className}>News</h2>
-            <Link className="link_more" href={'#'}>View all news</Link>
-          </div>
-        <div className="news-list">
-            <div className="news-item">
-                
+          <div className="block-main">
+            <div className="comm-title">
+              <h2 className={sharpieFont.className}>News</h2>
+              <Link className="link_more" href={"#"}>
+                View all news
+              </Link>
             </div>
+            <div className="news-list">
+              {showNews.map((news, index) => {
+                return (
+                  <div className="news-item" key={index}>
+                    <div className="news-head">
+                      <img src={news.head} />
+                    </div>
+                    <div className="news-content">
+                      <div className="news-time">
+                        <span>{news.time}</span>
+                        <span className="tag">{news.tag}</span>
+                      </div>
+                      <p>{news.title} </p>
+                      <div className="more">
+                        <Link href={news.link} target="_blank">
+                          Read more
+                        </Link>
+                      </div>
+                    </div>
+                  </div>
+                );
+              })}
+            </div>
+          </div>
         </div>
-          
-        
+        <div className="comm-block-2">
+          <div className="block-main">
+            <div className="comm-title">
+              <h2 className={sharpieFont.className}>News</h2>
+              <Link className="link_more" href={"#"}>
+                View all news
+              </Link>
+            </div>
+            <div className="news-list">
+              {showNews.map((news, index) => {
+                return (
+                  <div className="news-item" key={index}>
+                    <div className="news-head">
+                      <img src={news.head} />
+                    </div>
+                    <div className="news-content">
+                      <div className="news-time">
+                        <span>{news.time}</span>
+                        <span className="tag">{news.tag}</span>
+                      </div>
+                      <p>{news.title} </p>
+                      <div className="more">
+                        <Link href={news.link} target="_blank">
+                          Read more
+                        </Link>
+                      </div>
+                    </div>
+                  </div>
+                );
+              })}
+            </div>
+          </div>
         </div>
         <div className="footer-screen">
           <Footer />
         </div>
       </main>
-      
     </>
   );
 };
