a30b85ebc83ed0f36876f20c50f1f56efb88dfe0
Jack <k4667333@gmail.com>
2024-06-19 15:42:18 +0800

    debug

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,18 +4,7 @@ import { useEffect } from "react";
 // import nextI18NextConfig from '../next-i18next.config.js'
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
-  useEffect(() => {
-    function setVh() {
-      let vh = window.innerHeight * 0.01;
-      document.documentElement.style.setProperty("--vh", `${vh}px`);
-    }
-    // 初始设置
-    setVh();
-    // 监听窗口大小变化
-    window.addEventListener("resize", setVh);
-    // 清除事件监听器
-    return () => window.removeEventListener("resize", setVh);
-  }, []);
+
 
   return <Component {...pageProps} />;
 };
